df1e36f64f1fc667afb84aa8901fbf3873f8b6d1
Uğur <ugur.altnsy@gmail.com>
2022-06-13 23:19:21 +0300

    Initial Commit

---
+++ AppIconResizer/AppIconResizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppIconResizerApp.swift
+//  AppIconResizer
+//
+//  Created by UĞUR ALTINSOY on 13.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppIconResizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
